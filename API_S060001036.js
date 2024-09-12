095b1f7c38fcd4d17a21f4edc0614987ab18e861
Chris <38303979+pjwang0109@users.noreply.github.com>
2024-09-12 11:37:26 +0800

    Update API_S060001036.js

--- API_S060001036.js
+++ API_S060001036.js
@@ -1,17 +1,17 @@
-    // let userObj = getUidToken();
-    // let uid = userObj.uid;
-    // let token = userObj.token;
-    // console.log('uid', uid);
-    // console.log('token', token);
+ let userObj = getUidToken();
+ let uid = userObj.uid;
+ let token = userObj.token;
+ console.log('uid', uid);
+ console.log('token', token);
     
 
-    // let table = $('#resualtTable_S010_034_wrapper').find('table#resualtTable_S010_034').DataTable()
-    // let tableDatatable = $('#resualtTable_S010_034_wrapper').find('table#resualtTable_S010_034').DataTable()
-    // console.log('@1', table);
+ let table = $('#resualtTable_S010_034_wrapper').find('table#resualtTable_S010_034').DataTable()
+ let tableDatatable = $('#resualtTable_S010_034_wrapper').find('table#resualtTable_S010_034').DataTable()
+ console.log('@1', table);
 
-    // let data = new Object;
-    // apiS060001036(uid, token).done(function (jsondata) {
-    //     if (jsondata.rc == "M0000") {
-    //         console.log('gg', jsondata);
-    //     }
-    // })
+ let data = new Object;
+ apiS060001036(uid, token).done(function (jsondata) {
+     if (jsondata.rc == "M0000") {
+         console.log('gg', jsondata);
+     }
+ })
